f0e1c403845210e116428bebe6671f1b731d01d2
yannick.laurent0@gmail.com <yannick.laurent0@gmail.com@604582b4-41d6-b9a8-5ea0-3cebf1fed8cc>
2012-05-08 16:01:03 +0000

    QuiPrendraLeDernier step 1

--- NewbieContest/QuiPrendraLeDernier/QuiPrendraLeDernier.cpp
+++ NewbieContest/QuiPrendraLeDernier/QuiPrendraLeDernier.cpp
@@ -2,9 +2,25 @@
 #include <../../curl/include/curl/curl.h>
 #include <iostream>
 #include <string>
+#include <QRegExp>
+#include <QStringList>
+#include <stdlib.h>
+#include <sstream>
+
+
 using namespace std;
 
 string contents;
+
+void displayList(QStringList list, string ds)
+{
+	QString str = "";
+	for(int l=0;l<list.size(); l++)
+		str.append(list[l]);
+	cout << ds << " : " << endl;
+	cout << str.toStdString() <<endl;
+	
+}
 
 size_t handle_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
@@ -25,31 +41,44 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
   return written;
 }
 
-CURLcode curlParsing(string url, string proxy, string sessid, string cookie)
+void curlParsing(string url, string proxy, string sessid, string cookie)
 {
-	string cooky="SMFCookie89=",sess="PHPSESSID=";
-	sess+=sessid.c_str();
-	sess+="; path=/";
+	contents="";
+	string data ="";
+	string cooky="PHPSESSID=";
+	cooky+=sessid.c_str();
+	cooky+="; SMFCookie89=";
 	cooky+=cookie.c_str();
 	cooky+="; path=/";
-	CURL *curl;
+	
 	CURLcode res;
-
+	CURL *curl;
 	curl = curl_easy_init();
 	if(curl) {
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 		curl_easy_setopt(curl, CURLOPT_PROXY, proxy.c_str()); 
 		curl_easy_setopt(curl, CURLOPT_HEADER, 0);
-		curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0");
 		curl_easy_setopt(curl,CURLOPT_FOLLOWLOCATION,1);
 		  /* no progress meter please */
 		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
 		// Tell libcurl what function to call when it has data
         curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,handle_data);
-		curl_easy_setopt(curl, CURLOPT_COOKIE, sess.c_str());
+		//curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);//to view verbose output 
+		//curl_easy_setopt(curl, CURLOPT_POST, 1);//Tell libcurl to use POST
+		//curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+		curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
+		curl_easy_setopt(curl, CURLOPT_AUTOREFERER, true);
+		curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "gzip, deflate");
+		
+			
+		
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
 		curl_easy_setopt(curl, CURLOPT_COOKIE, cooky.c_str());
 		res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
+		
 	}
 	/*
 	if (res == 0)
@@ -57,14 +86,33 @@ CURLcode curlParsing(string url, string proxy, string sessid, string cookie)
 	else
 		cerr << "Error: " << res << endl;
 	*/
-	return res;
 }
 
 int main(void)
 {
-	CURLcode res = curlParsing("http://www.newbiecontest.org/epreuves/prog/prog14.php", "proxy.manitougroup.com:8080", "df3c56f0c809ab44cf77e84d5ce76df2", "a%3A4%3A%7Bi%3A0%3Bs%3A4%3A%227230%22%3Bi%3A1%3Bs%3A40%3A%224901e78221d00532261ece3f85d415f55ae454e9%22%3Bi%3A2%3Bi%3A1525681197%3Bi%3A3%3Bi%3A0%3B%7D");
-	//<TAG\b[^>]*>(.*?)</TAG>
 	
 	
+	QRegExp rx("<br><br>(.*)<br><br>", Qt::CaseInsensitive );
+	int pos = 0, numTas = 1, nbPions = 1;
+	QStringList list;
+	string url = "www.newbiecontest.org/epreuves/prog/prog14.php";
+	char buffer [33];
+	curlParsing(url, "proxy.manitougroup.com:8080", "df3c56f0c809ab44cf77e84d5ce76df2", "a%3A4%3A%7Bi%3A0%3Bs%3A4%3A%227230%22%3Bi%3A1%3Bs%3A40%3A%224901e78221d00532261ece3f85d415f55ae454e9%22%3Bi%3A2%3Bi%3A1525681197%3Bi%3A3%3Bi%3A0%3B%7D");
+	cout <<contents<< endl;
+	pos = rx.indexIn(contents.c_str());
+	list = rx.capturedTexts();
+	url = "www.newbiecontest.org/epreuves/prog/prog14.php?numtas=1&nbpions=5";
+	curlParsing(url, "proxy.manitougroup.com:8080", "df3c56f0c809ab44cf77e84d5ce76df2", "a%3A4%3A%7Bi%3A0%3Bs%3A4%3A%227230%22%3Bi%3A1%3Bs%3A40%3A%224901e78221d00532261ece3f85d415f55ae454e9%22%3Bi%3A2%3Bi%3A1525681197%3Bi%3A3%3Bi%3A0%3B%7D");
+	cout <<contents<< endl;
+	cout << endl;
+
+	
+
+	
+	url = "http://www.newbiecontest.org/epreuves/prog/prog14.php?numtas=";
+	url+= itoa(numTas,buffer,10);
+	url+="&nbpions=";
+	url+=itoa(nbPions,buffer,10);
+
 	return 0;
 }
