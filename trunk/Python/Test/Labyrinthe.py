da6bee539b89c4ccfc69c7f81707d2a24f428257
yannick.laurent0@gmail.com <yannick.laurent0@gmail.com@604582b4-41d6-b9a8-5ea0-3cebf1fed8cc>
2012-06-18 12:14:04 +0000

    OK V1

--- trunk/Python/Test/Labyrinthe.py
+++ trunk/Python/Test/Labyrinthe.py
@@ -1,4 +1,6 @@
-debug = True;
+from pprint import pprint as pp
+
+continuer = True;
 
 def move(pgameBoard, pposition, pdirection):
 	position = {}
@@ -11,7 +13,6 @@ def move(pgameBoard, pposition, pdirection):
 		directionTest = {"up":1, "right":2, "down":3, "left":4}
 		newDirection = {"up":"up", "right":"right", "down":"down", "left":"left"}
 	elif pdirection == "right":
-		if debug: print "coucou"
 		directionTest = {"up":2, "right":3, "down":4, "left":1}
 		newDirection = {"up":"right", "right":"down", "down":"left", "left":"up"}
 	elif pdirection == "down":
@@ -22,33 +23,32 @@ def move(pgameBoard, pposition, pdirection):
 		newDirection = {"up":"left", "right":"up", "down":"right", "left":"down"}
 		
 	
-	if(checkValue(position[directionTest["right"]]["y"], len(gameBoard), position[directionTest["right"]]["x"], len(gameBoard[0])) and checkPath(pgameBoard, position, directionTest, "right", "_")):
+	if(checkValue(position[directionTest["right"]]["y"], len(gameBoard), position[directionTest["right"]]["x"], len(gameBoard[0])) and (checkPath(pgameBoard, position, directionTest, "right", "_") or checkPath(pgameBoard, position, directionTest, "right", "1"))):
 		pgameBoard, pposition, pdirection = doMove(pgameBoard, pposition, pdirection, directionTest, newDirection, position, "right")
-		if debug: print "1"
-	elif(checkValue(position[directionTest["up"]]["y"], len(gameBoard), position[directionTest["up"]]["x"], len(gameBoard[0])) and checkPath(pgameBoard, position, directionTest, "up", "_")):
+	elif(checkValue(position[directionTest["up"]]["y"], len(gameBoard), position[directionTest["up"]]["x"], len(gameBoard[0])) and (checkPath(pgameBoard, position, directionTest, "up", "_") or checkPath(pgameBoard, position, directionTest, "up", "1"))):
 		pgameBoard, pposition, pdirection = doMove(pgameBoard, pposition, pdirection, directionTest, newDirection, position, "up")
-		if debug: print "2"
-	elif(checkValue(position[directionTest["left"]]["y"], len(gameBoard), position[directionTest["left"]]["x"], len(gameBoard[0])) and checkPath(pgameBoard, position, directionTest, "left", "_")):
+	elif(checkValue(position[directionTest["left"]]["y"], len(gameBoard), position[directionTest["left"]]["x"], len(gameBoard[0])) and (checkPath(pgameBoard, position, directionTest, "left", "_") or checkPath(pgameBoard, position, directionTest, "left", "1"))):
 		pgameBoard, pposition, pdirection = doMove(pgameBoard, pposition, pdirection, directionTest, newDirection, position, "left")
-		if debug: print "3"
 	else:
 		pgameBoard, pposition, pdirection = doMove(pgameBoard, pposition, pdirection, directionTest, newDirection, position, "down")
-		if debug: print "4"
 		
 	return pgameBoard, pposition, pdirection
 
 def doMove(pgameBoard, pposition, pdirection, directionTest, newDirection, position, move):
+	global continuer
+	
+	if (pgameBoard[position[directionTest[move]]["y"]][position[directionTest[move]]["x"]]) == "1":
+		continuer = False
 	
 	pdirection = newDirection[move]
 	pgameBoard[position[directionTest[move]]["y"]][position[directionTest[move]]["x"]] = "0"
 	pgameBoard[pposition["y"]][pposition["x"]] = "_"
 	pposition["x"] = position[directionTest[move]]["x"]
 	pposition["y"] = position[directionTest[move]]["y"]
-	
 	return pgameBoard, pposition, pdirection
 
-def checkValue(value1, value2, compar1, compar2):
-	return (value1 >= 0 and value1 < compar1 and value2 >= 0 and value2 < compar2)
+def checkValue(position_y, taille_tableau, position_x, taille_ligne):
+	return (position_y >= 0 and position_y < taille_tableau and position_x >= 0 and position_x < taille_ligne)
 
 
 def checkPath(pgameBoard, position, directionTest, case, compar):
@@ -72,16 +72,11 @@ with open("exemple.txt", "r") as file:
 				position["y"] = i
 			j+=1
 		i+=1
-		
-if debug: print position
 
-
-for line in gameBoard:
-	print line
-
-gameBoard, position, direction = move(gameBoard, position, direction)
-
-
+while continuer != False:
+	gameBoard, position, direction = move(gameBoard, position, direction)
+	pp(gameBoard)
+	raw_input("suite?")
 
 
 
